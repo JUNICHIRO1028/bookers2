df0e69214d9b80756948dba4b5a0ba18be2c2aa2
EC2 Default User <junjun091028@gmail.com>
2025-03-03 06:06:27 +0000

    guest

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get "home/about"=>"homes#about"
   devise_for :users
 
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
